a7117f5a61dd8a052f20efb6ed5c3712360e85e3
Zitan Salehin <zitan-74@dipti.com.bd>
2024-07-06 20:40:49 +0600

    all files

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://zitansalehin123:root123@book-store-mern.vamzxar.mongodb.net/?retryWrites=true&w=majority&appName=book-store-mern';
